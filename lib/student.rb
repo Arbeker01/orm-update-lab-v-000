a0ccb15b2d8277fc6e1b2e653a0ed834acb564af
Romeo Gono <romeogono1@aol.com>
2018-08-16 02:41:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,8 +65,8 @@ class Student
 
   def update
     sql = <<-SQL
-      UPDATE students 
-      SET name = ?, grade = ? 
+      UPDATE students
+      SET name = ?, grade = ?
       WHERE id = ?
     SQL
 
