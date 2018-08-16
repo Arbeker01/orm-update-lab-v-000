e68c2d5fc226cba0798f376695f06a3e7431d098
Romeo Gono <romeogono1@aol.com>
2018-08-16 02:39:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,8 @@ class Student
       self.update
     else
       sql = <<-SQL
-        INSERT INTO students (name, grade) VALUES (?, ?)
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
       SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
@@ -49,6 +50,17 @@ class Student
 
   def self.new_from_db(row)
    student = Student.create(row[1], row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE name = ?
+    SQL
+
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[1], result[2], result[0])
   end
 
 
